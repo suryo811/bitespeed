12a3b0b38577ceb1277cea19c87121d6b1c9b837
suryoday <suryodaykaushik@gmail.com>
2025-06-12 19:06:06 +0530

    add helper method, updated endpoint to find all contacts

--- src/identify.ts
+++ src/identify.ts
@@ -3,6 +3,11 @@ import prisma from "./config/db.js";
 
 const router = Router();
 
+// helper function to remove null, undefined, duplicated from array
+function unique<T>(arr: (T | null | undefined)[]): T[] {
+  return Array.from(new Set(arr.filter((v) => v != null)));
+}
+
 router.post("/", async (req: Request, res: Response): Promise<any> => {
   const { email, phoneNumber } = req.body;
 
@@ -11,7 +16,7 @@ router.post("/", async (req: Request, res: Response): Promise<any> => {
   }
 
   // 1. Find all contacts matching with email OR phoneNumber
-  const contacts = await prisma.contact.findMany({
+  const existingContacts = await prisma.contact.findMany({
     where: {
       OR: [email ? { email } : {}, phoneNumber ? { phoneNumber } : {}],
     },
@@ -22,7 +27,7 @@ router.post("/", async (req: Request, res: Response): Promise<any> => {
 
   //   console.log(contacts);
 
-  if (contacts.length === 0) {
+  if (existingContacts.length === 0) {
     // 2. If no match found, create a new primary contact
     const newContact = await prisma.contact.create({
       data: {
@@ -42,7 +47,35 @@ router.post("/", async (req: Request, res: Response): Promise<any> => {
     });
   }
 
-  return res.status(200).json({ status: "success", contacts });
+  // 3. Matches found. Find all contacts linked by email or phone
+  // collect all contacts with any matching email or phone from any of the found contacts.
+  let contactIds = existingContacts.map((c) => c.id);
+  let emailsToCheck = unique(existingContacts.map((c) => c.email));
+  let phonesToCheck = unique(existingContacts.map((c) => c.phoneNumber));
+
+  console.log(contactIds, emailsToCheck, phonesToCheck);
+
+  let allContacts = [...existingContacts];
+  let keepSearching = true;
+  while (keepSearching) {
+    const nextContacts = await prisma.contact.findMany({
+      where: {
+        OR: [{ email: { in: emailsToCheck } }, { phoneNumber: { in: phonesToCheck } }],
+      },
+    });
+    // Add any new contacts found
+    let beforeCount = allContacts.length;
+    for (const c of nextContacts) {
+      if (!contactIds.includes(c.id)) {
+        allContacts.push(c);
+        contactIds.push(c.id);
+        if (c.email && !emailsToCheck.includes(c.email)) emailsToCheck.push(c.email);
+        if (c.phoneNumber && !phonesToCheck.includes(c.phoneNumber))
+          phonesToCheck.push(c.phoneNumber);
+      }
+    }
+    keepSearching = allContacts.length > beforeCount;
+  }
 });
 
 export default router;
